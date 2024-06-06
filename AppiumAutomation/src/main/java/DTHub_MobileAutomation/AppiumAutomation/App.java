99ba172c260e45f8d855996b4f2f3b56a2d6a307
Shri Hari <Shri Hari@DESKTOP-5A5ROCQ>
2024-06-06 13:55:18 +0530

    Appium Project

---
+++ AppiumAutomation/src/main/java/DTHub_MobileAutomation/AppiumAutomation/App.java
@@ -0,0 +1,13 @@
+package DTHub_MobileAutomation.AppiumAutomation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
